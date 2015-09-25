c0a4de64434b063e89f3fae025ff28c64e1bdc18
Manu Phatak <bionikspoon@gmail.com>
2015-09-24 22:43:14 -0500

    cleanup

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,48 +1,5 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
-
-chrome.tabs.onUpdated.addListener(tabId => {
-  chrome.pageAction.show(tabId);
-});
-
-//chrome.webRequest.onBeforeRequest.addListener(request => {
-//  if (request.url.indexOf('?') !== -1) {
-//    let seperator;
-//    if (request.url.split('?')[0].endsWith('webhp')) {
-//      seperator = '#';
-//    } else {
-//      seperator = '?';
-//    }
-//    if (request.url.indexOf(seperator) === -1) {
-//      return;
-//    }
-//
-//    const url = request.url.split(seperator)[0];
-//    const payload = request.url
-//      .split(seperator)[1]
-//      .split('&')
-//      .reduce((obj, param) => obj.set(...param.split('=')), new Map());
-//
-//    if (payload.has('tbs')) {
-//      payload.set('tbs', 'qdr:y');
-//    }
-//    console.log('payload', payload);
-//
-//
-//    const payloadString = [...payload.entries()]
-//      .map((params) => params.join('='))
-//      .join('&');
-//
-//    console.log('redirecting to', url + seperator + payloadString);
-//
-//
-//    return {redirectUrl: url + seperator + payloadString};
-//
-//  }
-//}, {urls: ['*://*.google.com/search*']}, ['blocking']);
 
 const reGoogle = /^https?:\/\/(?:www\.)?google\.com\/(?:webhp|search)/;
 chrome.tabs.onUpdated.addListener((id, tab) => {
@@ -57,6 +14,7 @@ chrome.tabs.onUpdated.addListener((id, tab) => {
         .reduce((obj, param) => obj.set(...param.split('=')), new Map());
 
       if (payload.has('q') && payload.get('q').indexOf('ubuntu') !== -1) {
+        chrome.pageAction.show(id);
 
         if (payload.has('tbs') || payload.has('tbas')) {
           return;
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,50 +4,6 @@ var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = 
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId) {
-  chrome.pageAction.show(tabId);
-});
-
-//chrome.webRequest.onBeforeRequest.addListener(request => {
-//  if (request.url.indexOf('?') !== -1) {
-//    let seperator;
-//    if (request.url.split('?')[0].endsWith('webhp')) {
-//      seperator = '#';
-//    } else {
-//      seperator = '?';
-//    }
-//    if (request.url.indexOf(seperator) === -1) {
-//      return;
-//    }
-//
-//    const url = request.url.split(seperator)[0];
-//    const payload = request.url
-//      .split(seperator)[1]
-//      .split('&')
-//      .reduce((obj, param) => obj.set(...param.split('=')), new Map());
-//
-//    if (payload.has('tbs')) {
-//      payload.set('tbs', 'qdr:y');
-//    }
-//    console.log('payload', payload);
-//
-//
-//    const payloadString = [...payload.entries()]
-//      .map((params) => params.join('='))
-//      .join('&');
-//
-//    console.log('redirecting to', url + seperator + payloadString);
-//
-//
-//    return {redirectUrl: url + seperator + payloadString};
-//
-//  }
-//}, {urls: ['*://*.google.com/search*']}, ['blocking']);
-
 var reGoogle = /^https?:\/\/(?:www\.)?google\.com\/(?:webhp|search)/;
 chrome.tabs.onUpdated.addListener(function (id, tab) {
 
@@ -65,6 +21,7 @@ chrome.tabs.onUpdated.addListener(function (id, tab) {
       }, new Map());
 
       if (payload.has('q') && payload.get('q').indexOf('ubuntu') !== -1) {
+        chrome.pageAction.show(id);
 
         if (payload.has('tbs') || payload.has('tbas')) {
           return;
